5b64f8af79956bfcbb20caeadfea12296ba06fd5
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-05-13 16:17:51 +0600

    Basic Programming

--- 0_JAVAPROGRAMMINGBASICS/Patterns/Patterns.java
+++ 0_JAVAPROGRAMMINGBASICS/Patterns/Patterns.java
@@ -50,12 +50,32 @@ public class Patterns{
             System.out.println();
         }
     }
+    public static void k_pattern(int n) {
+    	
+    	int i,j;
+    	for( i=n; i>=1;i--) {
+    		for ( j=1; j<=i ;j++) {
+    			System.out.print("*");
+    			
+    		}
+    		System.out.println();
+    		
+    	}
+    	for (i=2; i<=n ;i++) {
+			for ( j=1;j<=i;j++) {
+    			System.out.print("*");
+    			
+    		}
+    		System.out.println();
+		}
+    }
 	
 	public static void main(String args[]) {
 		hollowRectangle(5, 5);
         inverted_rotated_halfPyramid(4);
         inverted_halfPyramid_withNumbers(5);
         floyds_triangle(5);
+        k_pattern(6);
 
 	}
 	
