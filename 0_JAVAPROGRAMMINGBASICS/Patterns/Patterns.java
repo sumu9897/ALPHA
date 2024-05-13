bded1a5c5de199b35cd4f13fac189f828a5f496d
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-05-13 14:14:33 +0600

    Basic Programming

---
+++ 0_JAVAPROGRAMMINGBASICS/Patterns/Patterns.java
@@ -0,0 +1,20 @@
+public class Patterns{
+	
+	public static void hollowRectangle(int n, int m) {
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=m; j++) {
+                if(i == 1 || j == 1 || i == n || j == m) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+	
+	public static void main(String args[]) {
+		hollowRectangle(5, 5);
+	}
+	
+}
